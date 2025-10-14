455a7d19e4aef2ecb19459ecf24c4a682ee5bfd9
Marco Ferraioli <marcoferraioli@live.com>
2025-10-14 13:44:28 +0200

    remove Neovim configuration files and related settings

--- .config/nvim/lua/plugins/lualine.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = function(_, opts)
-      -- hide clock from statusline, it's already in tmux
-      opts.sections.lualine_z = {}
-    end,
-  },
-}
