d890428be47dcdf2f9c1f26d9cd795e6ad329865
Marco Ferraioli <marcoferraioli@live.com>
2025-10-16 10:59:57 +0200

    feat: refactor

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = { hidden = true },
+        files = { hidden = true },
+      },
+    },
+  },
+}
